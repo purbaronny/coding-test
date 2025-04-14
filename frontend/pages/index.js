6e3875e3cfcecd784c00dec8e23e3fbc6914a3fc
purbaronny <ronny.purba@yahoo.co.id>
2025-04-14 22:20:34 +0700

    feat: change users to salesReps

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     fetch("http://localhost:8000/api/data")
       .then((res) => res.json())
       .then((data) => {
-        setUsers(data.users || []);
+        setUsers(data.salesReps || []);
         setLoading(false);
       })
       .catch((err) => {
